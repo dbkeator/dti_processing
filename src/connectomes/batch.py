c23a97a52879d9f92d7826fa511b7899b2fb5bf9
Steve Granger <grangers@uci.edu>
2021-09-03 12:11:47 -0700

    first edits adding dsi studio dictionaries for ease of use

--- src/connectomes/batch.py
+++ src/connectomes/batch.py
@@ -52,6 +52,50 @@ def main(argv):
         "--bval=":join("data","dwi.bval"),
         "--bvec=":join("data","dwi.bvec")
     }
+    # create source file
+    dsisource = {"dsi_studio --action=":"src",
+              "--source=":join("data","dwi.nii.gz"),
+              "--output=":join("data","src_base.src.gz") ,
+              "--bval=":join("data","dwi.bval"),
+              "--bvec=":join("data","dwi.bvec")
+              }
+    # check src file for quality
+    dsiquality = {"dsi_studio --action=":"qc",
+              "--source=":join("data","src_base_src.gz")
+              }
+    # reconstruct the images (create fib file; QSDR method=7,GQI method = 4)
+    dsirecon = {"dsi_studio --action=":"rec",
+              "--source=":join("data","src_base.src.gz"),
+              "--method=":join("data:","7"),
+              "--param0=":join("data","1.25"),
+              "--param1=":join("data","1"),
+              "--half_sphere=":join("data","1"),
+              "--odf_order=":join("data","8"),
+              "--num_fiber=":join("data","10"),
+              "--interpo_method=":join("data","0"),
+              "--scheme_balance=":join("data","1"),
+              "--check_btable=":join("data","1"), 
+              "--other_image=":joint("data","1w:T1.nii.gz")
+              }
+    # run robust tractography whole brain
+    dsiruntract = {"dsi_studio --action=":"trk",
+                   
+                   
+   
+  ##############Run whole brain Tractography with 10 million seeds 
+    dsi_studio --action= trk --method=0 --seed_count=10000000--threshold_index=qa --fa_threshold=0.00--initial_dir=0--seed_plan=0--interpolation=0--thread_count=12--step_size=0 --turning_angle=65 --smoothing=.6 --min_length=10 --max_length=600 --output=count_connect.trk.gz
+    ##############Run connectivity analysis with AAL atlas counting streamlines ending in regions 
+    dsi_studio --action= ana --tract=count_connect.trk.gz --connectivity=aal --connectivity_value=count --connectivity_type=end --output=connectivity_countmeasures.txt
+    ##############################################Visualize tracts
+    dsi_studio --action=vis --source=$arr --track="count_connect.trk.gz" --cmd="set_view,2+save_image,tractography1.jpg,1024 800"
+    dsi_studio --action=vis --source=$arr --track="count_connect.trk.gz" --cmd="slice_off+set_view,2+save_image,tractography2.jpg,1024 800"	
+    dsi_studio --action=vis --source=$arr --track="count_connect.trk.gz" --cmd="set_view,1+save_image,tractography3.jpg,1024 800"
+    dsi_studio --action=vis --source=$arr --track="count_connect.trk.gz" --cmd="slice_off+set_view,1+save_image,tractography4.jpg,1024 800"
+    ########################################################################################################
+    ####################################################Prepare data for FA Difference Maps (Longitudinal Data)####################################################
+    ########################################################################################################
+    #produce FA maps here
+    dtifit --data <scan.ii> -o FAdata -m <brain_mask.nii> -bvec *bvec -bval *bval
 
     dsistudio(source_dir=args.dir,out_dir=args.dir,logger=logger,kwargs=kwargs)
 
